14654b8a04070e26901ca692fbfa1acf661e475d
Matthew Rawlings <mattcanemail@gmail.com>
2021-05-11 23:57:47 -0700

    Additional dependencies and hooks

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,35 @@
 FROM debian:stable-slim
 LABEL maintainer="serverboi@serverboi.org"
 ENV DEBIAN_FRONTEND=noninteractive
-RUN apt-get update \
+RUN dpkg --add-architecture i386 \
+    && apt-get update \
     && apt-get -y --no-install-recommends install apt-utils \
     && apt-get -y dist-upgrade \
     && apt-get -y --no-install-recommends install \
+        libc6-dev \
+        lib32gcc1 \
+        lib32stdc++6 \
+        lib32z1 \
+        libsdl2-2.0-0 \
+        libsdl2-2.0-0:i386 \
+        locales \
         curl \
+        wget \
+        git \
+        python3-pip \
+        build-essential \
+        python3-dev \
         unzip \
         zip \
+        libcurl4 \
+        ca-certificates \
         python3-minimal \
         python3-pkg-resources \
-    && rm -f /bin/sh \
-    && ln -s /bin/bash /bin/sh \
     && update-alternatives --install /usr/bin/python python /usr/bin/python3 1 \
-    && apt-get clean \
-EXPOSE 63725
+    && mkdir -p /opt/serverboi/scripts \
+    && apt-get clean
+COPY patch_wf_embed.py /opt/serverboi/scripts/
+COPY hooks /usr/local/bin/
+RUN pip3 install setuptools \
+    && pip3 install git+https://github.com/Awlsring/ServerBoi-Utils.git
+EXPOSE 63725/tcp
---
+++ patch_wf_embed.py
@@ -0,0 +1,64 @@
+import os
+import click
+from discord import Color
+import serverboi_utils.embeds as embed_utils
+import serverboi_utils.responses as response_utils
+
+WORKFLOW_NAME = "Provision-Server"
+EXECUTION_NAME = os.environ.get("EXECUTION_NAME")
+
+APPLICATION_ID = os.environ.get("APPLICATION_ID")
+INTERACTION_TOKEN = os.environ.get("INTERACTION_TOKEN")
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option("-s", "--stage", "stage", required=True, type=str)
+def change_stage(stage: str):
+    embed = embed_utils.form_workflow_embed(
+        workflow_name=WORKFLOW_NAME,
+        workflow_description=f"Workflow ID: {EXECUTION_NAME}",
+        status="🟢 running",
+        stage=stage,
+        color=Color.green(),
+    )
+
+    data = response_utils.form_response_data(embeds=[embed])
+    response_utils.edit_response(APPLICATION_ID, INTERACTION_TOKEN, data)
+
+
+@cli.command()
+def finish_wf():
+    embed = embed_utils.form_workflow_embed(
+        workflow_name=WORKFLOW_NAME,
+        workflow_description=f"Workflow ID: {EXECUTION_NAME}",
+        status="✔️ finished",
+        stage="Complete",
+        color=Color.dark_green(),
+    )
+
+    data = response_utils.form_response_data(embeds=[embed])
+    response_utils.edit_response(APPLICATION_ID, INTERACTION_TOKEN, data)
+
+
+@cli.command()
+@click.option("-s", "--stage", "stage", required=True, type=str)
+def fail_wf(stage: str):
+    embed = embed_utils.form_workflow_embed(
+        workflow_name=WORKFLOW_NAME,
+        workflow_description=f"Workflow ID: {EXECUTION_NAME}",
+        status="❌ failed",
+        stage=stage,
+        color=Color.red(),
+    )
+
+    data = response_utils.form_response_data(embeds=[embed])
+    response_utils.edit_response(APPLICATION_ID, INTERACTION_TOKEN, data)
+
+
+if __name__ == "__main__":
+    cli()
